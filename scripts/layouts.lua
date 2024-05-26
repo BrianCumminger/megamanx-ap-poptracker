c95dffd56bae889483c5db2c76cfe9ac59e1a46d
MeridianBC <brian.cumminger@gmail.com>
2024-05-26 01:15:24 -0300

    Added stage maps

--- scripts/layouts.lua
+++ scripts/layouts.lua
@@ -5,4 +5,5 @@ if (string.find(Tracker.ActiveVariantUID,"standard")) then
     Tracker:AddLocations("locations/locations.json")
     Tracker:AddLayouts("layouts/broadcast.json")
     Tracker:AddLayouts("layouts/tracker.json")
+    Tracker:AddLayouts("layouts/layouttabs.json")
 end
